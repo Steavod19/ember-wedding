397ee4f055fdddfeb42f6a6e139ed9f604fc3d8d
Steve Rogers <srogers.design@gmail.com>
2015-10-11 01:05:39 -0400

    added images and navbaar css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-bootstrap': {
-        'importBootstrapTheme': false
+        'importBootstrapTheme': true
     }
   });
 
