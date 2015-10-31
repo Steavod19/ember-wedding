996d7b00f7de2f6db4cb74552d763bc705f114c1
Steve Rogers <srogers.design@gmail.com>
2015-10-30 21:55:43 -0400

    logistics page

--- app/controllers/party/bride.js
+++ app/controllers/party/bride.js
@@ -14,22 +14,22 @@ export default Ember.Controller.extend({
 
     bonnaCall: function() {
       this.setProperties({'name': "Bonna", 'location': "Paraguay", 'from': 'Helena, MT',
-      'affiliation': 'Bride', 'info': 'its Bonna', 'img': 'https://s3.amazonaws.com/wedding-site-images/bonnaprofile.png'})
+      'affiliation': 'Bride', 'info': 'its Bonna', 'img': 'https://s3.amazonaws.com/wedding-site-images/bonnaprofile.png'});
     },
     joannaCall: function() {
       this.setProperties({'name': "JoAnna", 'location': "Helena, MT", 'from': 'Helena, MT',
       'affiliation': 'Bride', 'img': 'https://s3.amazonaws.com/wedding-site-images/JoVery.jpg',
-      'info': 'Joanna'})
+      'info': 'Joanna'});
     },
     hawleyCall: function() {
       this.setProperties({'name': "Hawley", 'location': "Worcester, MA", 'from': 'RENO!',
       'affiliation': 'Bride', 'img': 'https://s3.amazonaws.com/wedding-site-images/hawley.jpg',
-      'info': 'its Hawley'})
+      'info': 'its Hawley'});
     },
     kimCall: function() {
       this.setProperties({'name': "Kim", 'location': "Spokane, WA", 'from': 'Helena, MT',
       'affiliation': 'groom', 'img': 'https://s3.amazonaws.com/wedding-site-images/Kimmie.jpg',
-      'info': 'its Kim'})
+      'info': 'its Kim'});
     }
   }
 });
--- app/controllers/party/groom.js
+++ app/controllers/party/groom.js
@@ -14,27 +14,27 @@ export default Ember.Controller.extend({
 
     steveCall: function() {
       this.setProperties({'name': "Steve", 'location': "MA", 'from': 'Weymouth, MA',
-      'affiliation': 'Groom', 'info': 'Steve Mac and I grew up across the street from each other in Weymouth. We met in kindergarden and also went to CSC together. He also broke my nose once', 'img': 'https://s3.amazonaws.com/wedding-site-images/stevemac.png'})
+      'affiliation': 'Groom', 'info': 'Steve Mac and I grew up across the street from each other in Weymouth. We met in kindergarden and also went to CSC together. He also broke my nose once', 'img': 'https://s3.amazonaws.com/wedding-site-images/stevemac.png'});
     },
     jeffCall: function() {
       this.setProperties({'name': "Jeff", 'location': "Somewhere, GA", 'from': 'NH(?)',
       'affiliation': 'Groom', 'img': 'https://s3.amazonaws.com/wedding-site-images/jeff.jpg',
-      'info': 'Classes from Berts to Babs. Jeff \'was\' the trouble of CSC. He doesn\'t date boys that wear red hats because he is one.'})
+      'info': 'Classes from Berts to Babs. Jeff \'was\' the trouble of CSC. He doesn\'t date boys that wear red hats because he is one.'});
     },
     trentCall: function() {
       this.setProperties({'name': "Trent", 'location': "Milwaukee", 'from': 'Burlington, VT',
       'affiliation': 'Groom', 'img': 'https://s3.amazonaws.com/wedding-site-images/trent.jpg',
-      'info': 'Another art nerd with me. Trent was a pretty good designer and he had a sweet man bun for a while.'})
+      'info': 'Another art nerd with me. Trent was a pretty good designer and he had a sweet man bun for a while.'});
     },
     adamCall: function() {
       this.setProperties({'name': "Adam", 'location': "Newburyport, MA", 'from': 'NHish',
       'affiliation': 'Groom', 'img': 'https://s3.amazonaws.com/wedding-site-images/adam.jpg',
-      'info': 'My freshman roommate at CSC. My usual partner in crime except when he\'s stealing snacks from the lodge'})
+      'info': 'My freshman roommate at CSC. My usual partner in crime except when he\'s stealing snacks from the lodge'});
     },
     peteCall: function() {
       this.setProperties({'name': "Peter", 'location': "HipsterLand, VT", 'from': 'Vergennes, VT',
       'affiliation': 'Groom', 'img': 'https://s3.amazonaws.com/wedding-site-images/peter.jpg',
-      'info': 'Dependable Petey a Graphic designer with me. Last to go to bed, first at church the next morning, probably because he takes so many naps. '})
+      'info': 'Dependable Petey a Graphic designer with me. Last to go to bed, first at church the next morning, probably because he takes so many naps. '});
     }
   }
 });
