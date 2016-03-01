2f14123b7ca5fae76c7407a0221229b3048f284a
Steve Rogers <srogers.design@gmail.com>
2016-03-01 11:36:57 -0500

    added sm scrn menu

---
+++ app/controllers/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isSmNav: false,
+  actions: {
+    smNavClick: function() {
+      console.log(this.toggleProperty('isSmNav'));
+    },
+
+
+  },
+});
